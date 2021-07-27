e89bae324737a2de8b5d23591ceb93920a4ba229
FabianMassotto <massotto.fabian@gmail.com>
2021-07-26 18:13:03 -0700

    Initial files

---
+++ lpc1769_wifi/inc/esp01.h
@@ -0,0 +1,8 @@
+
+#ifndef ESP01_H_
+#define ESP01_H_
+
+/* LPCOpen dependency */
+#include "chip.h"
+
+#endif /* ESP01_H_ */
---
+++ lpc1769_wifi/src/main.c
@@ -0,0 +1,19 @@
+
+#include "esp01.h"
+
+
+int main(void) {
+
+
+    SystemCoreClockUpdate();
+
+    volatile static int i = 0 ;
+    // Enter an infinite loop, just incrementing a counter
+    while(1) {
+        i++ ;
+        // "Dummy" NOP to allow source level single
+        // stepping of tight while() loop
+        __asm volatile ("nop");
+    }
+    return 0 ;
+}
